680331cd0ebd494d51f71c53f35316401f10c331
noor <noor@noor-pc>
2019-01-20 12:22:17 -0500

    before adding JFrame

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -5,27 +5,43 @@ import java.awt.Graphics;
 import acm.graphics.*;
 import acm.program.*;
 import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 import java.lang.Object;
 
 public class Pong extends GraphicsProgram {
-	private static final int RADIUS = 50;
-	private static final int BACKGROUND_WIDTH = 1000;
+	private static final int RADIUS = 35;
+	private static final int BACKGROUND_WIDTH = 1200;
 	private static final int BACKGROUND_HEIGHT = 600;
 	private static final int PAUSE_TIME = 40;
-
 	GOval ball;
 	GLabel player1;
 	GLabel player2;
+	int p1Score = 0;
+	int p2Score = 0;
+
 	public void run() {
+//		try { 
+//		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//		} catch (Exception e) {
+//		    e.printStackTrace();
+//		}
 		createBackGround();
 		createBall();
+		createPaddles();
 		addScores();
 		moveBall();
-		//getLabel() might be of use 
+		// getLabel() might be of use
 	}
 
 	public void createBackGround() {
-		setSize(BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+//		setSize(BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+		JFrame frame = new JFrame();
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		frame.setUndecorated(true);
+		frame.setVisible(true);
 		setBackground(Color.decode("#8c8786"));
 
 	}
@@ -34,27 +50,31 @@ public class Pong extends GraphicsProgram {
 		ball = new GOval(RADIUS, RADIUS);
 		ball.setFilled(true);
 		ball.setColor(Color.decode("#31fa00"));
-		add(ball, getWidth() / 2 - RADIUS/2, getHeight() / 2 - RADIUS/2);
+		add(ball, getWidth() / 2 - RADIUS / 2, getHeight() / 2 - RADIUS / 2);
 	}
+
 	public void moveBall() {
-		pause(1500); //wait for the text to be displayed
+		pause(1500); // wait for the text to be displayed
 		Random rand = new Random();
 		int dx = rand.nextInt(1) + 3;
 		int dy = rand.nextInt(3) + 5;
 		int i = 0;
-		while(i < 1000) {
-			// <= since we're moving in increments that are higher than 1 so we might skip the point where ball.getX is equal to getWidth
-			if(getWidth() <= ball.getX() + RADIUS || ball.getX()<= 0) {
+		while (i < 1000) {
+			// <= since we're moving in increments that are higher than 1 so we might skip
+			// the point where ball.getX is equal to getWidth
+			if (getWidth() <= ball.getX() + RADIUS || ball.getX() <= 0) {
 				dx = -dx;
 			}
-			if(getHeight() <= ball.getY() + RADIUS || ball.getY() <= 0) {
+			if (getHeight() <= ball.getY() + RADIUS || ball.getY() <= 0) {
 				dy = -dy;
 			}
 			ball.move(dx, dy);
+			increaseScore();
 			pause(5);
 			i++;
 		}
 	}
+
 	public void addScores() {
 
 		player1 = new GLabel("0");
@@ -63,8 +83,23 @@ public class Pong extends GraphicsProgram {
 		player2.setColor(Color.green);
 		player1.setFont(new Font("TimesRoman", Font.PLAIN, 24));
 		player2.setFont(new Font("TimesRoman", Font.PLAIN, 24));
-		add(player1, getWidth()/4 - player1.getWidth()/2, 40);
-		add(player2, getWidth()*0.73 , 40);
+		add(player1, getWidth() / 4 - player1.getWidth() / 2, 40);
+		add(player2, getWidth() * 0.73, 40);
 	}
 
+	public void increaseScore() {
+
+
+		if (getWidth() <= ball.getX() + RADIUS ) {
+			p1Score++;
+			player1.setLabel(p1Score+"");
+		}
+		if(ball.getX() <= 0) {
+			p2Score++;
+			player2.setLabel(p2Score+"");
+		}
+	}
+	public void createPaddles() {
+		GRect paddle1 = new GRect(20, 50);
+	}
 }
